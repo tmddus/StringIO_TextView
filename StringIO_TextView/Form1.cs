305542e7af07ffe52d1e2b77370437f2fcecae85
Seungyeon Song <song99pro@gmail.com>
2019-05-07 11:59:14 +0900

    #2 입력 내용이 빈칸인 경우 에러메시지를 띄움

--- StringIO_TextView/Form1.cs
+++ StringIO_TextView/Form1.cs
@@ -9,6 +9,7 @@ using System.Windows.Forms;
 
 namespace StringIO_TextView
 {
+
     public partial class Form1 : Form
     {
 
@@ -32,7 +33,27 @@ namespace StringIO_TextView
 
         private void button1_Click(object sender, EventArgs e)
         {
-            this.lblResult.Text = this.OrgStr + this.txtEdit.Text;
+            if (TextCheck()){
+                this.lblResult.Text = this.OrgStr + this.txtEdit.Text;
+            }
+         
         }
+
+        private bool TextCheck()
+        {
+            if (this.txtEdit.Text != "")
+            {
+                return true;
+            }
+            else
+            {
+                MessageBox.Show("텍스트를 입력하세요!", "알림",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                this.txtEdit.Focus();
+                return false;
+            }
+        }
+
     }
+
 }
